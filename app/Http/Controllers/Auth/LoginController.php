79fcffff6978e67181d126ca682b5086692ac024
thomasmcculloch2 <112406448+thomasmcculloch2@users.noreply.github.com>
2022-09-20 15:46:46 -0300

    Feature/#3fm5f95/login (#2)
    * feat: register
    
    * bug
    
    * debugging
    
    * feat: login
    
    * git ignore fix
    
    * delete file
    
    Co-authored-by: = <=>

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\JsonResponse;
+
+class LoginController
+{
+    public function __invoke(LoginRequest $request): JsonResponse
+    {
+        $data =  $request->validated();
+
+        $user = User::where('email',$data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'],$user->password))  { //@TODO convert the password into a hash value in resgisterController when creating a user
+            return response()->json(['message' => 'Invalid Credentials'], 401);
+        } else {
+            $token = $user->createToken('userToken')->plainTextToken;
+            $response = [
+                'message' => 'Login successful',
+                'user' => $user,
+                'token' => $token
+            ];
+
+            return response()->json($response, 201);
+        }
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,14 +17,21 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
+        $number = rand(1,2);
+
+        $user = [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'username' => fake()->unique()->userName(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+        if ($number == 1) {
+            $user['type'] = 'doctor';
+        } else {
+            $user['type'] = 'patient';
+        }
+        return $user;
     }
 
     /**
