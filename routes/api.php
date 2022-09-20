f3d1375eb39e2f5a0ace3fef751abdc68cdd51f7
= <=>
2022-09-20 13:54:39 -0300

    feat: login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', RegisterController::class)->name('user.register');
+
+Route::post('login', LoginController::class)->name('user.login');
---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use \App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    public function loginIsSuccess():void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson(route('user.login'), [
+            'email' => $user->email,
+            'password' => $user->password
+        ]);
+
+        $response->assertSuccessful()
+            ->assertJsonStructure([
+                'user',
+                'token'
+            ]);
+    }
+
+    public function loginWithInvalidEmailOrPassword() :void
+    {
+        $this->postJson(route('user.login'), [
+            'email' => 'invalid@mail.com',
+            'password' => 'password'
+        ])->assertUnauthorized();
+    }
+
+    public function loginWithoutEmail() :void
+    {
+        $this->postJson(route('user.login'), [
+            'email' => '',
+            'password' => 'password'
+        ])->assertUnprocessable();
+    }
+
+    public function loginWithoutPassword() :void
+    {
+        $this->postJson(route('user.login'), [
+            'email' => 'mail@mail.com',
+            'password' => ''
+        ])->assertUnprocessable();
+    }
+
+    public function loginWithoutValidEmail() :void
+    {
+        $this->postJson(route('user.login'), [
+            'email' => 'mail',
+            'password' => 'password'
+        ])->assertUnprocessable();
+    }
+}
