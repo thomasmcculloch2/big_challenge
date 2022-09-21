16d2dfbb536b31ed766dbd0a8adaf5e4086d57b1
thomasmcculloch2 <112406448+thomasmcculloch2@users.noreply.github.com>
2022-09-21 09:01:26 -0300

    fix: register bugs (#4)
    * fix: register bugs
    
    * fix: import hash
    
    Co-authored-by: = <=>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,12 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController
 {
     public function __invoke(RegisterRequest $request): JsonResponse
     {
-        $user =  User::create($request->validated());
+        $data = $request->validated();
+        $user =  User::create([
+            'name'=> $data['name'],
+            'email'=> $data['email'],
+            'password'=> Hash::make($data['password']),
+            'type'=> $data['type'],
+        ]);
 
         $token = $user->createToken('userToken')->plainTextToken;
 
