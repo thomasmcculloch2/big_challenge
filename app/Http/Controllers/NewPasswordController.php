f22bf374a0370b2c56c803d8b20b0041161d4290
= <=>
2022-10-03 10:49:34 -0300

    feat: forgot password

---
+++ app/Http/Controllers/NewPasswordController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NewPasswordRequest;
+use Hash;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+class NewPasswordController
+{
+    private Dispatcher $dispatcher;
+
+    public function __construct(Dispatcher $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+    }
+    public function __invoke(NewPasswordRequest $request): JsonResponse
+    {
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user) use ($request) {
+                $user->forceFill([
+                    'password' => Hash::make($request->password),
+                    'remember_token' => Str::random(60),
+                ])->save();
+
+                $user->tokens()->delete();
+                $this->dispatcher->dispatch(new PasswordReset($user));
+            }
+        );
+
+        if ($status == Password::PASSWORD_RESET) {
+            return response()->json([
+                'message' => 'Password reset successfully'
+            ]);
+        }
+
+        return response()->json([
+            'message' => __($status)
+        ], 500);
+    }
+}
