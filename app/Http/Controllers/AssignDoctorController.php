3b10b9212da92b9fc0a145355e02ab4e5bddf9dc
= <=>
2022-10-04 09:24:26 -0300

    fix: change status when a doctor is assigned

--- app/Http/Controllers/AssignDoctorController.php
+++ app/Http/Controllers/AssignDoctorController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Constants\SubmissionStatus;
 use App\Models\Submission;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
@@ -16,7 +17,8 @@ class AssignDoctorController
             return response()->json(['message' => 'Submission already have a doctor'], 401);
         }
         $submission->update([
-            'doctor_id' => Auth::id()
+            'doctor_id' => Auth::id(),
+            'status' => SubmissionStatus::IN_PROGRESS
         ]);
         return response()->json(['message' => 'Doctor associated successfully'], 201);
     }
